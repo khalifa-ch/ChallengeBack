879e02321f99fb9e6977fe77983acc2361eb87ae
khalifa-ch <khalifachelbi06@gmail.com>
2024-06-28 22:05:25 +0100

    fixEnvFile

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 
 const app: Express = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("hello there from express hey  ");
+  res.send("hello there from express with typescipt ");
 });
 const port = 5000;
 app.listen(port, () => {
-  console.log(`server is running on ${port}`);
+  console.log(`server is running on  ${process.env.PORT}`);
 });
