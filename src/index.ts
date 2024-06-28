3df1537e1a161ed58798462bfed5763cc19fb8ef
khalifa-ch <khalifachelbi06@gmail.com>
2024-06-28 22:21:57 +0100

    fixMongoConnection

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
-
-
 const app: Express = express();
+app.use(express.json()); // JSON data
+
+
+mongoose.connect(process.env.DB as string)
+.then(()=>{
+    console.log('Mongodb connected')
+}).catch(err=>{
+    console.log(err)
+})
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hello there from express with typescipt ");
 });
-const port = 5000;
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`server is running on  ${process.env.PORT}`);
 });
